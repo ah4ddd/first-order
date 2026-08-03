926f0ce505461dbc273af69f055b7fa27b81b662
Ahad <mohdahad60080@gmail.com>
2026-08-03 13:07:36 +0530

    add login and index HTML pages, update CORS settings, and add VS Code settings for live server

--- app/main.py
+++ app/main.py
@@ -25,7 +25,14 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=[
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "http://127.0.0.1:5501",    # ← this is the one
+    "http://localhost:5501",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
